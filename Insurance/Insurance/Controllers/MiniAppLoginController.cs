91fc017533e64ee339a9616ae9bfd42630f90db8
dugujian2321 <49682530+dugujian2321@users.noreply.github.com>
2021-03-01 23:16:34 +0800

    添加小程序登录Controller

---
+++ Insurance/Insurance/Controllers/MiniAppLoginController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VirtualCredit;
+using MiniApp.Helper;
+using VirtualCredit.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Insurance.Controllers
+{
+    [ApiController]
+    [Route("mini/[controller]")]
+    public class MiniAppLoginController : MiniAppControllerBase
+    {
+        [Route("[action]")]
+        public async Task<IActionResult> Login(string userName, string password, string openId)
+        {
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage str = await httpClient.GetAsync($@"https://api.weixin.qq.com/sns/jscode2session?appid=wxabac8813c1db09ae&secret=SECRET&js_code={openId}&grant_type=authorization_code");
+                       
+            LoginReturnModel result = new LoginReturnModel();
+            IUser tryLoginUser = new UserInfoModel();
+            tryLoginUser.UserName = userName;
+            tryLoginUser.userPassword = password;
+            var uim = DatabaseService.UserMatchUserNamePassword(tryLoginUser);
+            if (uim is null)
+            {
+                result.Message = "用户名或密码错误";
+            }
+            else
+            {
+                result.Message = "登录成功";
+                OnlineUsers.Add(uim);
+            }
+
+            return Ok(result);
+        }
+    }
+
+    public class LoginReturnModel
+    {
+        public string Message { get; set; }
+    }
+}
