53999af3a84b7a8479eb437779c907ae19a6dc57
dugujian2321 <49682530+dugujian2321@users.noreply.github.com>
2021-03-13 23:24:24 +0800

    小程序前端： 添加ocr组件

--- MiniAppFrontEnd/miniprogram/pages/add/add.js
+++ MiniAppFrontEnd/miniprogram/pages/add/add.js
@@ -1,66 +1,39 @@
-// pages/add/add.js
+//var base64 = require("../images/base64");
 Page({
-
   /**
-   * 页面的初始数据
-   */
+ * 页面的初始数据
+ */
   data: {
-
+    list_add:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    this.setData({
+      //icon: base64.icon20,
+      slideButtons: [{
+        text: '普通',
+        //src: '/page/weui/cell/icon_love.svg', // icon的路径
+      }, {
+        text: '普通',
+        extClass: 'test',
+        //src: '/page/weui/cell/icon_star.svg', // icon的路径
+      }, {
+        type: 'warn',
+        text: '警示',
+        extClass: 'test',
+        //src: '/page/weui/cell/icon_del.svg', // icon的路径
+      }],
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  success: function (e) {
+    var k = {
+      x:"1",
+      y:"2"
+    }
+    this.data.list_add.push(k)
+    
+    console.log(e)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  slideButtonTap(e) {
+    console.log('slide button tap', e.detail)
   }
-})
+});
